b0d74ed6ea956f41c993b6e0fd5dcf5c7970a31d
c0d3-404 <98340031+c0d3-404@users.noreply.github.com>
2022-08-28 16:05:50 +0100

    added template

---
+++ ValtersVariousClasses/ValtersVariousClasses.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using ValtersVariousClasses.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+
+namespace ValtersVariousClasses
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class ValtersVariousClasses : BaseUnityPlugin
+    {
+        
+        private const string ModId = "com.CrimzonBlyum.rounds.ValtersVariousClasses";
+        private const string ModName = "Valter's Various Classes";
+        public const string Version = "0.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "VVC";
+
+        public static ValtersVariousClasses? instance { get; private set; }
+
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            instance = this;
+            CustomCard.BuildCard<CardName>();
+        }
+    }
+
+}
